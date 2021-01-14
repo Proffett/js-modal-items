c1dda84511565453112675c6061b733edd715fca
Evgeny <evgeny.profet@gmail.com>
2021-01-14 16:42:08 +0300

    added carousel slider, css changed

--- js/script.js
+++ js/script.js
@@ -319,10 +319,11 @@ window.addEventListener("DOMContentLoaded", function () {
     }, 4000);
   }
 
-  //SLIDER second variant
+  //SLIDER second variant with dot's nav
   const slides = document.querySelectorAll(".offer__slide"),
+    slider = document.querySelector(".offer__slider"),
     prev = document.querySelector(".offer__slider-prev"),
-    next = document.querySelector(".offer__slider-next");
+    next = document.querySelector(".offer__slider-next"),
     total = document.querySelector("#total"),
     current = document.querySelector("#current"),
     slidesWrapper = document.querySelector(".offer__slider-wrapper"),
@@ -332,41 +333,62 @@ window.addEventListener("DOMContentLoaded", function () {
     let slideIndex = 1;
     let offset = 0;
 
-    if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-    current.textContent = `0${slideIndex}`;
-    } else {
-      total.textContent = slides.length;
-      current.textContent = slideIndex;
-    };
-
   slidesField.style.width = 100 * slides.length + "%";
   slidesField.style.display = "flex";
   slidesField.style.transition = "0.5s all";
-
   slidesWrapper.style.overflow = "hidden";
+  slides.forEach(slide => { slide.style.width = width; });
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol'),
+        dots = [];
+  indicators.classList.add('carousel-indicators');
+  indicators.classList.add('indicators');
+  slider.append(indicators);
+  for(let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add('dots');
+    if (i == 0) {
+      dot.style.opacity = 1;
+    };
+    indicators.append(dot);
+    dots.push(dot);
+  };
 
-  slides.forEach((slide) => {
-    slide.style.width = width;
-  });
+  function checkZeroIndicator(current, total) {
+    if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+      current.textContent = `0${slideIndex}`;
+    } else {
+      total.textContent = slides.length;
+      current.textContent = slideIndex;
+    }
+  };
+  function makeDotsOpacity(dotsArray) {
+    try {
+      dotsArray.forEach((dot) => (dot.style.opacity = ".5"));
+    } finally {
+      dotsArray[slideIndex - 1].style.opacity = 1;
+    };
+  };
+
+  checkZeroIndicator(current, total);
+
   next.addEventListener("click", () => {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
       offset = 0;
     } else {
       offset += +width.slice(0, width.length - 2);
-    }
+    };
     slidesField.style.transform = `translateX(-${offset}px)`;
 
     if(slideIndex == slides.length) {
       slideIndex =1;
     } else {
       slideIndex++;
-    }
-    if(slides.length < 10) {
-      current.textContent = `0${slideIndex}`
-    } else {
-      current.textContent = slideIndex;
-    }
+    };
+    checkZeroIndicator(current, total);
+    makeDotsOpacity(dots);
   });
 
   prev.addEventListener("click", () => {
@@ -381,12 +403,21 @@ window.addEventListener("DOMContentLoaded", function () {
       slideIndex = slides.length;
     } else {
       slideIndex--;
-    }
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    };
+    checkZeroIndicator(current, total);
+    makeDotsOpacity(dots);
+  });
+
+  dots.forEach(dot => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      checkZeroIndicator(current, total);
+      makeDotsOpacity(dots);
+    });
   });
 
 
@@ -407,7 +438,7 @@ window.addEventListener("DOMContentLoaded", function () {
   //     slideIndex = slides.length;
   //   }
 
-  //   slides.forEach((item) => (item.style.display = "none"));
+  //   slides.forEach(item => (item.style.display = "none"));
   //   slides[slideIndex - 1].style.display = "block";
 
   //   if (slides.length < 10) {
