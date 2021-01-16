c22475cb0da831bce56b4fda42f5e8d95c40b844
Evgeny <evgeny.profet@gmail.com>
2021-01-16 23:51:48 +0300

    interpolation with modules, webpack added

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+import tabs from "./modules/tabs";
+import timer from "./modules/timer";
+import modal from "./modules/modal";
+import cards from "./modules/cards";
+import forms from "./modules/forms";
+import slider from "./modules/slider";
+import calc from "./modules/calc";
+import {openModal} from './modules/modal'
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  const modalTimerId = setTimeout(
+    () => openModal(".modal", modalTimerId),
+    300000
+  );
+
+  tabs(
+    ".tabheader__item",
+    ".tabcontent",
+    ".tabheader__items",
+    "tabheader__item_active"
+  );
+  timer(".timer", "2021-02-11");
+  modal("[data-modal]", ".modal", modalTimerId);
+  cards();
+  forms('form', modalTimerId);
+  slider({
+    container: ".offer__slider",
+    nextArrow: ".offer__slider-next",
+    prevArrow: ".offer__slider-prev",
+    slide: ".offer__slide",
+    totalCounter: "#total",
+    currentCounter: "#current",
+    wrapper: ".offer__slider-wrapper",
+    field: ".offer__slider-inner",
+  });
+  calc();
+  
+});
