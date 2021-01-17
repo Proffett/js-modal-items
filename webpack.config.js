b29215f88cfe2b758c3b0652ce5e79410057ba9f
Evgeny <evgeny.profet@gmail.com>
2021-01-17 14:10:29 +0300

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,37 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/script.js',
+  mode: "production",
+  entry: "./src/js/script.js",
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/js'
+    filename: "bundle.js",
+    path: __dirname + "/js",
   },
   watch: true,
 
   devtool: "source-map",
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  debug: true,
+                  corejs: 3,
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
